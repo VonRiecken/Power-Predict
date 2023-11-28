3d4355328f7bc20cf3ec2c456ac02077f1acf9c8
john riecken <johnriecken94@gmail.com>
2023-11-28 17:45:18 +0100

    project structure

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
