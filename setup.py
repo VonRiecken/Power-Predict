7ed7bfb753f3016cf3d2ed6424fbc793f5fd4690
john riecken <johnriecken94@gmail.com>
2023-11-29 12:53:17 +0100

    initial app initialising

--- setup.py
+++ setup.py
@@ -4,3 +4,11 @@ from setuptools import setup
 with open("requirements.txt") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if "git+" not in x]
+
+
+setup(name='power_project',
+      version='0.0.0',
+      author='John Riecken',
+      install_requires=requirements,
+      packages=find_packages()
+      )
