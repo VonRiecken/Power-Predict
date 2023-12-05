a8a7da24d8b878354a9d89c425ecf9429d671a85
Bryan Rohena <bryanrohena@gmail.com>
2023-12-05 14:44:58 +0100

    Data Processing

--- power_predict/models/function_linear_regression.py
+++ power_predict/models/function_linear_regression.py
@@ -1,15 +1,54 @@
 import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from power_predict.logic.data import load_local_df
+from sklearn.model_selection import cross_val_score
 #from sklearn.externals import joblib
 
+# --- Fetching Data ---
+df = df = load_local_df()
+#df = pd.read_csv('/Users/bryan/code/VonRiecken/Power-Predict/power_predict/data/merged_dataset2023-12-01 18:08:35.062618.csv')
+
+df.head(5)
+
+# --- Data Preprocessing ---
+# Setting Country + Month year as Index
+df['Country_Month'] = df['Country'] + '_' + df['Month_year'].astype(str)
+df = df.set_index('Country_Month')
+# Separating features and target variables
+X = df.drop(['Unnamed: 0', 'Month_year', 'Balance',
+             'Combustible_Renewables', 'Hydro', 'Other_Renewables', 'Solar',
+             'Total_Renewables__Hydro__Geo__Solar__Wind__Other_', 'Wind',
+             'total_sol_wind_hyd', 'value_CDD_18', 'value_CDD_21',
+             'value_HDD_16', 'value_HDD_18', 'value_Heat_index',], axis=1)
+# Applying logistic (log) transformation to the target variables
+y = np.log1p(df[['Hydro', 'Solar', 'Wind', 'total_sol_wind_hyd']])
+# Init list of numerical columns
+num_features = X.select_dtypes(include=[np.number]).columns.tolist()
+# Preprocessing pipeline
+preprocessing_pipeline = ColumnTransformer(
+    transformers=[
+        ('num', Pipeline(steps=[
+            ('scaler', MinMaxScaler())
+        ]), num_features),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), ['Country'])
+    ])
+# Splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
+
+# --- Create Model ----
 def run_linear_regression_model():
     df = load_local_df()
+    #df = pd.read_csv('/Users/bryan/code/VonRiecken/Power-Predict/power_predict/data/merged_dataset2023-12-01 18:08:35.062618.csv')
+
 
     # Preprocessing
     final = df
@@ -19,7 +58,12 @@ def run_linear_regression_model():
         raise ValueError("No columns with 'value' in their names.")
 
     X = final[columns_X]
-    columns_to_drop = ['value_CDD_18', 'value_CDD_21', 'value_HDD_16', 'value_HDD_18', 'value_Heat_index']
+    columns_to_drop = ['value_CDD_18',
+                       'value_CDD_21',
+                       'value_HDD_16',
+                       'value_HDD_18',
+                       'value_Heat_index'
+                       ]
     X = X.drop(columns=columns_to_drop)
 
     # Feature scaling
@@ -27,16 +71,29 @@ def run_linear_regression_model():
     X_scaled = scaler.fit_transform(X)
 
     # Here you can choose which technology output to predict
-    #y = final['total_sol_wind_hyd']
+    y = final['total_sol_wind_hyd']
     #y = final['Solar']
-    y = final['Wind']
-    # y = final['Hydro']
+    #y = final['Wind']
+    #y = final['Hydro']
 
     # Split training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
     # Linear regression model
     model = LinearRegression()
+
+    # Cross-validation
+    cv_scores = cross_val_score(model, X_scaled, y, cv=5, scoring='neg_mean_squared_error')
+    mse_cv_mean = -cv_scores.mean()
+
+    print(f'Mean Cross-validated Mean Squared Error: {mse_cv_mean}')
+
+    # Fit the model on the training set
+    model.fit(X_train, y_train)
+
+    # Predict on the test set
+    y_pred = model.predict(X_test)
+
 
     # Print the model type
     print('Model Type: Linear Regression')
@@ -57,7 +114,7 @@ def run_linear_regression_model():
     print(f'R-squared: {r2}')
 
     return model
-
+"""""
 #save_model(run_linear_regression_model, 'Linear Regression')
 
 #def save_model(model, model_type):
@@ -68,4 +125,6 @@ def run_linear_regression_model():
 # Run the linear regression model and save it
 #linear_regression_model = run_linear_regression_model()
 #save_model(linear_regression_model, 'Linear Regression')
+"""
+
 run_linear_regression_model()
