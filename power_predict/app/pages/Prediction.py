521d0e9cf5485537240b84f1bf39770fc0a5d0fd
john riecken <johnriecken94@gmail.com>
2023-12-07 02:41:27 +0100

    gcloud service url

--- power_predict/app/pages/Prediction.py
+++ power_predict/app/pages/Prediction.py
@@ -67,10 +67,10 @@ elif target == 'Total':
     add_bg_from_local('power_predict/app/total.jpg')
 
 #  local
-api_url_ = 'http://127.0.0.1:8000/predict'
+# api_url_ = 'http://127.0.0.1:8000/predict'
 
 # cloud image
-# api_url_ = 'https://stage1-irosqzxbhq-ew.a.run.app/predict'
+api_url_ = 'https://prod-irosqzxbhq-ew.a.run.app/predict'
 # 'https://mvp-irosqzxbhq-ew.a.run.app/predict'
 
 
